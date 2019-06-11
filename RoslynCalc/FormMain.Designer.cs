6a5136ef86468d7624c5e33f8939d1b4d107554a
sim511777 <sim51177@gmail.com>
2019-06-11 13:33:17 +0900

    Initial upload

---
+++ RoslynCalc/FormMain.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace RoslynCalc {
+   partial class FormMain {
+      /// <summary>
+      /// 필수 디자이너 변수입니다.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// 사용 중인 모든 리소스를 정리합니다.
+      /// </summary>
+      /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+      protected override void Dispose(bool disposing) {
+         if (disposing && (components != null)) {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form 디자이너에서 생성한 코드
+
+      /// <summary>
+      /// 디자이너 지원에 필요한 메서드입니다. 
+      /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+      /// </summary>
+      private void InitializeComponent() {
+         this.lbxResult = new System.Windows.Forms.ListBox();
+         this.tbxExpression = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // lbxResult
+         // 
+         this.lbxResult.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.lbxResult.FormattingEnabled = true;
+         this.lbxResult.ItemHeight = 12;
+         this.lbxResult.Location = new System.Drawing.Point(0, 21);
+         this.lbxResult.Name = "lbxResult";
+         this.lbxResult.Size = new System.Drawing.Size(429, 377);
+         this.lbxResult.TabIndex = 0;
+         this.lbxResult.SelectedIndexChanged += new System.EventHandler(this.lbxResult_SelectedIndexChanged);
+         // 
+         // tbxExpression
+         // 
+         this.tbxExpression.Dock = System.Windows.Forms.DockStyle.Top;
+         this.tbxExpression.Location = new System.Drawing.Point(0, 0);
+         this.tbxExpression.Name = "tbxExpression";
+         this.tbxExpression.Size = new System.Drawing.Size(429, 21);
+         this.tbxExpression.TabIndex = 1;
+         this.tbxExpression.Text = "3*(1.2+2)";
+         this.tbxExpression.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxExpression_KeyDown);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(429, 398);
+         this.Controls.Add(this.lbxResult);
+         this.Controls.Add(this.tbxExpression);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.ListBox lbxResult;
+      private System.Windows.Forms.TextBox tbxExpression;
+   }
+}
+
---
+++ RoslynCalc/FormMain.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoslynCalc {
+   public partial class FormMain : Form {
+      public FormMain() {
+         InitializeComponent();
+      }
+
+      private async void Eval() {
+         string exp = this.tbxExpression.Text;
+         string output = string.Empty;
+         try {
+            object res = await CSharpScript.EvaluateAsync(exp);
+            output = exp + " = " + res.ToString();
+         } catch (Exception ex) {
+            output = exp + " = " + ex.Message;
+         }
+         this.lbxResult.Items.Insert(0, output);
+         this.tbxExpression.Clear();
+      }
+
+      private void tbxExpression_KeyDown(object sender, KeyEventArgs e) {
+         if (e.KeyCode == Keys.Enter) {
+            this.Eval();
+         }
+      }
+
+      private void lbxResult_SelectedIndexChanged(object sender, EventArgs e) {
+         string text = this.lbxResult.SelectedItem.ToString();
+         string exp = text.Split(new string[]{" = "}, StringSplitOptions.None)[0];
+         this.tbxExpression.Text = exp;
+      }
+   }
+}
